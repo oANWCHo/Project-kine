c856ca9680c9d8da2d8e36d5e3eb5eb604e985d7
Nanoo <92351791+oANWCHo@users.noreply.github.com>
2024-11-20 23:34:09 +0700

    Update Frame

--- autorun.m
+++ autorun.m
@@ -1,36 +1,14 @@
-R = 1;
-
-% Set the model name
-model = 'hand'; % Replace 'hand' with your Simulink model name
-
-% Load the Simulink model (optional if already loaded)
-load_system(model);
-
-% Set simulation time to infinity (so it can run indefinitely)
-set_param(model, 'StopTime', 'inf');
-
-% Start the simulation
-set_param(model, 'SimulationCommand', 'start');
-
-% Wait for 1 seconds
-pause(1);
-
-% Stop the simulation
-set_param(model, 'SimulationCommand', 'stop');
-
-% Close the Simulink model (optional)
-close_system(model, 0); % The '0' ensures unsaved changes are not discarded
-
-disp('Simulation stopped after 10 seconds.');
-
-R = 2;
-
-load_system(model);
-
-set_param(model, 'SimulationCommand', 'start');
-
-pause(1);
-
-set_param(model, 'SimulationCommand', 'stop');
-
-disp('Simulation stopped after 0 seconds.');
+R = 1.25;
+L = R + 0.25;
+model = 'hand';
+while R < 5
+     load_system(model);
+     set_param(model, 'StopTime', 'inf');
+     set_param(model, 'SimulationCommand', 'start');
+     while out.simout.Data(end,:) == 0
+         disp("dwodowkd")
+     end
+     set_param(model, 'SimulationCommand', 'stop');
+     close_system(model, 0);
+     R = R + 0.01;
+end
