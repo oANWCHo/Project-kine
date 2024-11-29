1a147f00d9877a4027d75716a3376514ecf931f5
Nanoo <92351791+oANWCHo@users.noreply.github.com>
2024-11-30 04:29:20 +0700

    renew keep sphere and cylindical

--- autorun.m
+++ autorun.m
@@ -1,44 +1,38 @@
-<<<<<<< Updated upstream
 R = 3.25;
-=======
-Radius_C = 0.5;
-Length_C = 1;
+Radius_C = 0.6;
+Length_C = 6;
 offset = 0;
->>>>>>> Stashed changes
 model = 'hand';
 buttonName = 'Push Button1';
 blockPath = 'hand/To File';
 
-<<<<<<< Updated upstream
-while R < 5
-     load_system(model);
-     fileName = sprintf('sphere_%f.mat', R);
-     set_param(blockPath, 'FileName', fileName);
-     set_param(model, 'StopTime', '10');
-     set_param(model, 'SimulationCommand', 'start');
-     % set_param([model '/' buttonName], 'Value', '1');
-=======
-while Length_C < 4
-     load_system(model);
-     fileName = sprintf('Cylindical_L%f.mat', Length_C);
-     set_param(blockPath, 'FileName', fileName);
-     set_param(model, 'StopTime', '10');
-     set_param(model, 'SimulationCommand', 'start');
->>>>>>> Stashed changes
+% while R < 5
+%      load_system(model);
+%      fileName = sprintf('sphere_%f.mat', R);
+%      set_param(blockPath, 'FileName', fileName);
+%      set_param(model, 'StopTime', '10');
+%      set_param(model, 'SimulationCommand', 'start');
+%      % set_param([model '/' buttonName], 'Value', '1');
+
+while Radius_C < 4
+      load_system(model);
+      fileName = sprintf('Cylindical_R%f.mat', Radius_C);
+      set_param(blockPath, 'FileName', fileName);
+      set_param(model, 'StopTime', '10');
+      set_param(model, 'SimulationCommand', 'start');
+ 
      status = get_param(model, 'SimulationStatus');
      while ~strcmp(status, 'stopped')
          status = get_param(model, 'SimulationStatus');
          disp('Simulation has reached the stop time or stopped.');
          pause(1);
      end 
-<<<<<<< Updated upstream
      % set_param([model '/' buttonName], 'Value', '0');
-     set_param(model, 'SimulationCommand', 'stop');
-     % close_system(model, 0);
-     R = R + 0.05;
+%      set_param(model, 'SimulationCommand', 'stop');
+%      close_system(model, 0);
+%      R = R + 0.05;
+% end
+      set_param(model, 'SimulationCommand', 'stop');
+      Radius_C = Radius_C + 0.05;
+      % Length_C = Length_C + 0.05;
 end
-=======
-     set_param(model, 'SimulationCommand', 'stop');
-     Length_C = Length_C + 0.05;
-end
->>>>>>> Stashed changes
