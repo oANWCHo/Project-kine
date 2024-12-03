429a21d67451f9af3f3d9e96b5f9fda09438b411
Nanoo <92351791+oANWCHo@users.noreply.github.com>
2024-12-03 15:07:05 +0700

    Cylindical L data

--- autorun.m
+++ autorun.m
@@ -1,6 +1,7 @@
 R = 3.25;
-Radius_C = 0.6;
-Length_C = 6;
+Radius_C = 1;
+% Length_C = 2.4;
+Length_C = 4.05;
 offset = 0;
 model = 'hand';
 buttonName = 'Push Button1';
@@ -14,9 +15,11 @@ blockPath = 'hand/To File';
 %      set_param(model, 'SimulationCommand', 'start');
 %      % set_param([model '/' buttonName], 'Value', '1');
 
-while Radius_C < 4
+% while Radius_C < 4
+while Length_C < 8
       load_system(model);
-      fileName = sprintf('Cylindical_R%f.mat', Radius_C);
+      % fileName = sprintf('Cylindical_R%f.mat', Radius_C);
+      fileName = sprintf('Cylindical_R%f.mat', Length_C);
       set_param(blockPath, 'FileName', fileName);
       set_param(model, 'StopTime', '15');
       set_param(model, 'SimulationCommand', 'start');
@@ -33,6 +36,6 @@ while Radius_C < 4
 %      R = R + 0.05;
 % end
       set_param(model, 'SimulationCommand', 'stop');
-      Radius_C = Radius_C + 0.05;
-      % Length_C = Length_C + 0.05;
+      % Radius_C = Radius_C + 0.05;
+      Length_C = Length_C + 0.05;
 end
